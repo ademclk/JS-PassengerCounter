6d0251cb296e56e25b2fa7768c140e5d37c9245c
ademclk <ademonurcelik@gmail.com>
2022-06-29 00:37:24 +0300

    onclick event listener added for increment button.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-document.getElementById("count").innerText=0;
+// document.getElementById("count").innerText=0;
+
+// initialize count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML
+
+function increment() {
+    console.log("The button was clicked.");
+}
+
