42d075079785ffae593a9cfef07e30abf28bb369
ademclk <ademonurcelik@gmail.com>
2022-06-30 00:33:13 +0300

    Save button added.

--- index.js
+++ index.js
@@ -10,8 +10,13 @@ let countEl = document.getElementById("count-el")
 console.log(countEl)
 
 let count = 0
-
+// Create a function that will increment the count variable by 1
 function increment() {
     count = count + 1
     countEl.innerText = count
 }
+// Create a function that logs out the count variable
+function save(){
+    console.log(count)
+}
+
