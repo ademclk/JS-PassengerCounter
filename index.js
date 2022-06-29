c03d667aacd4ba3bdbf3c7ef02695ebe46275029
ademclk <ademonurcelik@gmail.com>
2022-06-29 23:55:18 +0300

    Button has been made useful.

--- index.js
+++ index.js
@@ -5,9 +5,13 @@
 // increment the count variable when the button is clicked
 // change the count-el in the HTML
 
-let count = 0
-function increment() {
-    count = count + 1;
-    console.log(count)
-}
+let countEl = document.getElementById("count-el")
 
+console.log(countEl)
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
